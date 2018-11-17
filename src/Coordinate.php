12174168a4f0aa3092b78ba3ba6141138d07a356
Ilya Lifshits <ilya.tau@gmail.com>
2018-11-17 08:04:17 +0200

    Add toString method to Coordinate class

--- src/Coordinate.php
+++ src/Coordinate.php
@@ -25,5 +25,10 @@ class Coordinate
         $this->longitude = $longitude;
     }
 
+    public function __toString() : string
+    {
+       return $this->latitude+','+$this->latitude;
+    }
+
 
 }
